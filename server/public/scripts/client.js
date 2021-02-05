ac7eb87c1aa278e311d1b32d26bbce4daf74858d
Johnshandsable <Jshands@asu.edu>
2021-02-05 16:45:31 -0600

    figured out a way to store the operation

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,36 @@
 console.log('JS loaded');
 
 $(document).ready(function () {
+  console.log('jQuery loaded');
   // EVENT HANDLERS
-
   $(document).on('click', '#submitOperation', onSubmit);
+  $(document).on('click', '.operationButton', setOperation);
+  $(document).on('click', '#clearButton', clearEntries);
 });
 
+let operation = ''; // holds the operation
+
 function onSubmit(event) {
-  event.preventDefault();
+  event.preventDefault(); // prevent page from refreshing
   console.log('in onSubmit() ');
-  //$();
+
+  let x = $('#xInput').val();
+  let y = $('#yInput').val();
+  console.log('x, ', x);
+  console.log('y, ', y);
+
+  // let operation = $(this).data('operation');
+}
+
+function setOperation(event) {
+  event.preventDefault(); // prevent page from refreshing
+  console.log('in setOperation() ');
+  console.log($(this).data('operation'));
+  operation = $(this).data('operation');
+}
+
+function clearEntries() {
+  console.log('in clearEntries() ');
+  $('#xInput').val('');
+  $('#yInput').val('');
 }
