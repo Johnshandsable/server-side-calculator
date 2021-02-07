2b7f5b93ce9a6ddfee61c88a11013a57d08e4dbb
Johnshandsable <Jshands@asu.edu>
2021-02-07 09:55:44 -0600

    added a function which clears history of stored operations

--- server/modules/storage.js
+++ server/modules/storage.js
@@ -3,6 +3,7 @@ let storedResults = [];
 // Adds to the global storedResults
 function addToStoredResults(objectData) {
   storedResults.push(objectData);
+  return true;
 }
 
 // Returns the global storedResults
@@ -10,7 +11,13 @@ function getStoredResults() {
   return storedResults;
 }
 
+function clearStoredResults() {
+  storedResults = [];
+  return true;
+}
+
 module.exports = {
   addToStoredResults,
   getStoredResults,
+  clearStoredResults,
 };
